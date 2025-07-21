165c62d42dd586506d8e8546103b2eafb9780e1c
Mahbuba Islam <mahbubaislam7010@gmail.com>
2025-07-21 17:23:52 -0400

    coupon-applied

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ for(const cartBtn of cartBtns){
           selectedPlayer.appendChild(selectedPlayerDetails)
            totalCost('total-cost', playerPrice)
          
-          totalCost('grand-total', playerPrice)
+          // totalCost('grand-total', playerPrice)
           
- })
+ })   
 }
 
 
 const totalCost = (id, playerPrice) => {
- const totalCostElement = document.getElementById(id).innerText
+ const totalCostElement = document.getElementById(id).innerText 
           const totalCostValue = parseInt(totalCostElement)
           const totalPrice = parseInt(playerPrice.replace(/[^0-9]/g, ''));
 
@@ -31,9 +31,36 @@ const totalCost = (id, playerPrice) => {
            setInnerText('total-cost', totalCost)
            setInnerText('grand-total', totalCost)
 }
+
 const setInnerText = (id, value) => {
   document.getElementById(id).innerText = value
 
+}
+
+const applyCoupon = () => {
+  const couponInput = document.getElementById('couponInput').value
+  const discountMessage = document.getElementById('discount-message')
+ 
+  // sample coupon ideas
+  const coupons = {
+   "mahbuba20":20
+  }
+  if(coupons[couponInput]){
+    const discount = coupons[couponInput]
+    console.log(discount)
+    const discountValue = discount/100
+    console.log(discountValue)
+    const grandTotal = document.getElementById('grand-total').innerText
+    const grandTotalValue = parseFloat(grandTotal)
+    const grandTotalAfterDiscount = grandTotalValue - (grandTotalValue * discountValue);
+   setInnerText('grand-total', grandTotalAfterDiscount)
+    discountMessage.innerText = `coupon applied ! you got ${discount} % off`
+    document.getElementById('couponInput').value = ''
+  }
+  else{
+    discountMessage.innerHTML = `<span class="text-red-500 font-bold text-xl">X !</span>
+     invalid coupon code please try again`
+  }
 }
 
 // const getSelectedPlayerDetails = (e,index) => {
